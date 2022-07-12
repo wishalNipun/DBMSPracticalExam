95d7c16238fffabca614aef4c1533e609bc271e3
wishalNipun <wishal1974@gmail.com>
2022-07-12 11:32:51 +0530

    add loadAllStudents in StudetControllerForm

--- DBMSPracticalExam/src/controller/StudentFormController.java
+++ DBMSPracticalExam/src/controller/StudentFormController.java
@@ -2,9 +2,15 @@ package controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import model.Student;
+import view.tm.StudentTM;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class StudentFormController {
     public JFXTextField txtStudentId;
@@ -24,7 +30,34 @@ public class StudentFormController {
     public JFXTextField txtNIC;
     public JFXButton btnSearch;
     public TextField txtSearch;
+    private final StudentCRUDController studentCRUDController = new StudentCRUDController();
+    public void initialize(){
+        loadAllCustomer();
+    }
 
+    private void loadAllCustomer() {
 
+        try {
+            ArrayList<Student> allStudent = studentCRUDController.getAllStudent();
+            for (Student s:allStudent
+                 ) {
+                tblStudent.getItems().add(
+                        new StudentTM(s.getId(),s.getName(),s.getEmail(),s.getContact(),s.getAddress(),s.getNic())
+                );
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
 }
