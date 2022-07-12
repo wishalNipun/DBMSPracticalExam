360a0bedf3b62aa35ee07c3e8c42619920241f42
wishalNipun <wishal1974@gmail.com>
2022-07-12 11:10:20 +0530

    add attributes in StudentFormcontroller

--- DBMSPracticalExam/src/controller/StudentFormController.java
+++ DBMSPracticalExam/src/controller/StudentFormController.java
@@ -1,4 +1,29 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class StudentFormController {
+    public JFXTextField txtStudentId;
+    public JFXTextField txtName;
+    public TableView tblStudent;
+    public TableColumn colStudentId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colAddress;
+    public TableColumn colContact;
+    public TableColumn colNIC;
+    public JFXTextField txtContact;
+    public JFXButton btnSave;
+    public JFXButton btnDelete;
+    public JFXTextField txtAddress;
+    public JFXTextField txtEmail;
+    public JFXTextField txtNIC;
+    public JFXButton btnSearch;
+    public TextField txtSearch;
+
+    
 }
