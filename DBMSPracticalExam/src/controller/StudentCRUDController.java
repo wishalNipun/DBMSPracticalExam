8b9b1c2809c6f40f7e8e007955a247111775a72e
wishalNipun <wishal1974@gmail.com>
2022-07-12 11:26:14 +0530

    added getAllStudent in StudentCrudController

--- DBMSPracticalExam/src/controller/StudentCRUDController.java
+++ DBMSPracticalExam/src/controller/StudentCRUDController.java
@@ -1,5 +1,28 @@
 package controller;
 
-public class StudentCRUDController {
+import model.Student;
+import util.CrudUtil;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class StudentCRUDController {
+    public ArrayList<Student> getAllStudent() throws SQLException, ClassNotFoundException {
+        ArrayList <Student> students = new ArrayList<>();
+        ResultSet result = CrudUtil.executeQuery("SELECT * FROM Student");
+        while (result.next()){
+            students.add(
+                    new Student(
+                            result.getString(1),
+                            result.getString(2),
+                            result.getString(3),
+                            result.getString(4),
+                            result.getString(5),
+                            result.getString(6)
+                    )
+            );
+        }
+        return students;
+    }
 }
